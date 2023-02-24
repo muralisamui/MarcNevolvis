7060744a8f2545c623e133a9fe66ca4984da229d
muralisamui <muralisamui997@gmail.com>
2023-02-24 12:57:31 +0530

    changes 24-02-2023

--- Updated_Code/Wrmb_CompanyFunction.js
+++ Updated_Code/Wrmb_CompanyFunction.js
@@ -6,6 +6,54 @@ if (!this.Wrm) {
 Wrm.Company = function (executionContext) {
     "strict mode";
 
+    async function checkIdentificationDocumentsAsync(DocTypeToCheck) {
+        console.log("api called");
+        if (DocTypeToCheck !== null && DocTypeToCheck !== "") {
+            var docTypes = DocTypeToCheck.split(",");
+            console.log("api called 1");
+            if (docTypes.length > 0) {
+                var fetchDocTypeCheck =
+                    "<fetch mapping='logical'>" +
+                    "  <entity name='wrmb_identificationdocument'>" +
+                    "	<attribute name='wrmb_identificationdocumentid' />" +
+                    "	<filter type='and'>" +
+                    "	  <condition attribute='statecode' operator='eq' value='0' />" +
+                    "	  <condition attribute='wrmb_companyid' operator='eq' value='" + formContext.data.entity.getId() + "' />" +
+                    "	</filter>" +
+                    "	<link-entity name='wrmb_identificationdocumenttype' from='wrmb_identificationdocumenttypeid' to='wrmb_typeid' alias='aq'>" +
+                    "	  <filter type='or'>";
+
+                for (var i = 0; i < docTypes.length; i++) {
+                    fetchDocTypeCheck += "<condition attribute='wrmb_name' operator='eq' value='" + docTypes[i].trim() + "' />";
+                }
+
+                fetchDocTypeCheck += "</filter>" +
+                    "	</link-entity>" +
+                    "  </entity>" +
+                    "</fetch>";
+
+                try {
+                    const retrievedDocs = await new Promise((resolve, reject) => {
+                        XrmServiceToolkit.Soap.Fetch(fetchDocTypeCheck, null, (result) => {
+                            resolve(result);
+                            console.log("api called 2");
+                        }, (error) => {
+                            reject(error);
+                            console.log("api called 3");
+                        });
+                    });
+
+                    if (retrievedDocs === null || retrievedDocs.length === 0) {
+                        formContext.ui.setFormNotification('No valid Identification Document assigned to this record!', 'WARNING', 'identitydocument_NOTIFY');
+                    }
+                } catch (error) {
+                    console.log(error);
+                    console.log("api called 4");
+                }
+            }
+        }
+    }
+
     var BankTypeName = null;
     var onLoad = function (executionContext) {
         "strict mode";
@@ -25,49 +73,55 @@ Wrm.Company = function (executionContext) {
             }
             if (formContext.getAttribute("wrmb_isclient") !== null && formContext.getAttribute("wrmb_isclient").getValue() === true) {
                 Wrm.Common.GetWrmSetting("WRM.IdentificationDocument.CompanyIdentification.Types",
-                    async function checkIdentificationDocumentsAsync(DocTypeToCheck) {
-                        if (DocTypeToCheck !== null && DocTypeToCheck !== "") {
-                            var docTypes = DocTypeToCheck.split(",");
-                            if (docTypes.length > 0) {
-                                var fetchDocTypeCheck =
-                                    "<fetch mapping='logical'>" +
-                                    "  <entity name='wrmb_identificationdocument'>" +
-                                    "	<attribute name='wrmb_identificationdocumentid' />" +
-                                    "	<filter type='and'>" +
-                                    "	  <condition attribute='statecode' operator='eq' value='0' />" +
-                                    "	  <condition attribute='wrmb_companyid' operator='eq' value='" + formContext.data.entity.getId() + "' />" +
-                                    "	</filter>" +
-                                    "	<link-entity name='wrmb_identificationdocumenttype' from='wrmb_identificationdocumenttypeid' to='wrmb_typeid' alias='aq'>" +
-                                    "	  <filter type='or'>";
+                    //   async function checkIdentificationDocumentsAsync(DocTypeToCheck) {
+                    //     console.log("api called");
+                    //     if (DocTypeToCheck !== null && DocTypeToCheck !== "") {
+                    //         var docTypes = DocTypeToCheck.split(",");
+                    //         console.log("api called 1");
+                    //         if (docTypes.length > 0) {
+                    //             var fetchDocTypeCheck =
+                    //                 "<fetch mapping='logical'>" +
+                    //                 "  <entity name='wrmb_identificationdocument'>" +
+                    //                 "	<attribute name='wrmb_identificationdocumentid' />" +
+                    //                 "	<filter type='and'>" +
+                    //                 "	  <condition attribute='statecode' operator='eq' value='0' />" +
+                    //                 "	  <condition attribute='wrmb_companyid' operator='eq' value='" + formContext.data.entity.getId() + "' />" +
+                    //                 "	</filter>" +
+                    //                 "	<link-entity name='wrmb_identificationdocumenttype' from='wrmb_identificationdocumenttypeid' to='wrmb_typeid' alias='aq'>" +
+                    //                 "	  <filter type='or'>";
 
-                                for (var i = 0; i < docTypes.length; i++) {
-                                    fetchDocTypeCheck += "<condition attribute='wrmb_name' operator='eq' value='" + docTypes[i].trim() + "' />";
-                                }
+                    //             for (var i = 0; i < docTypes.length; i++) {
+                    //                 fetchDocTypeCheck += "<condition attribute='wrmb_name' operator='eq' value='" + docTypes[i].trim() + "' />";
+                    //             }
 
-                                fetchDocTypeCheck += "</filter>" +
-                                    "	</link-entity>" +
-                                    "  </entity>" +
-                                    "</fetch>";
+                    //             fetchDocTypeCheck += "</filter>" +
+                    //                 "	</link-entity>" +
+                    //                 "  </entity>" +
+                    //                 "</fetch>";
 
-                                try {
-                                    const retrievedDocs = await new Promise((resolve, reject) => {
-                                        XrmServiceToolkit.Soap.Fetch(fetchDocTypeCheck, null, (result) => {
-                                            resolve(result);
-                                        }, (error) => {
-                                            reject(error);
-                                        });
-                                    });
+                    //             try {
+                    //                 const retrievedDocs = await new Promise((resolve, reject) => {
+                    //                     XrmServiceToolkit.Soap.Fetch(fetchDocTypeCheck, null, (result) => {
+                    //                         resolve(result);
+                    //                         console.log("api called 2");
+                    //                     }, (error) => {
+                    //                         reject(error);
+                    //                         console.log("api called 3");
+                    //                     });
+                    //                 });
 
-                                    if (retrievedDocs === null || retrievedDocs.length === 0) {
-                                        formContext.ui.setFormNotification('No valid Identification Document assigned to this record!', 'WARNING', 'identitydocument_NOTIFY');
-                                    }
-                                } catch (error) {
-                                    console.log(error);
-                                }
-                            }
-                        }
-                    }
+                    //                 if (retrievedDocs === null || retrievedDocs.length === 0) {
+                    //                     formContext.ui.setFormNotification('No valid Identification Document assigned to this record!', 'WARNING', 'identitydocument_NOTIFY');
+                    //                 }
+                    //             } catch (error) {
+                    //                 console.log(error);
+                    //                 console.log("api called 4");
+                    //             }
+                    //         }
+                    //     }
+                    // }
 
+                    checkIdentificationDocumentsAsync()                 
                 );
             }
         }
@@ -98,6 +152,76 @@ Wrm.Company = function (executionContext) {
                 "wrmb_taxdom_city",
                 "wrmb_taxdom_state"]);
     };
+
+    // var onLoad = function (executionContext) {
+    //     let formContext = executionContext.getFormContext();
+	// 	if(formContext.getAttribute("wrmb_companytypeid") != null){
+	// 		BankTypeName = Wrm.Common.GetWrmSetting("WRM.Company.Bank.Type");
+	// 		formContext.getAttribute("wrmb_companytypeid").addOnChange(Wrm.Company.OnChangeCompanyType);
+	// 		Wrm.Company.CheckBankTab(formContext);
+	// 	}
+	// 	if(formContext.ui.getFormType() == 2 || formContext.ui.getFormType() == 3 || formContext.ui.getFormType() == 4 || formContext.ui.getFormType() == 11){
+	// 		if(formContext.getAttribute("wrmb_namecheck") != null && formContext.getAttribute("wrmb_namecheck").getValue() == true){
+	// 			formContext.ui.setFormNotification('Active Name Check on this record detected!', 'WARNING', 'wrmb_namecheck_NOTIFY');
+	// 		}
+	// 		if(formContext.getAttribute("wrmb_isclient") != null && formContext.getAttribute("wrmb_isclient").getValue() == true){
+    //             Wrm.Common.GetWrmSetting("WRM.IdentificationDocument.CompanyIdentification.Types")
+    //                 .then(async (DocTypeToCheck) => {
+    //                     await checkIdentificationDocumentsAsync(DocTypeToCheck, formContext);
+    //                 })
+    //                 .catch((error) => {
+    //                     console.log(error);
+    //                 });
+	// 		}
+	// 	}
+    // }
+    
+    async function checkIdentificationDocumentsAsync(DocTypeToCheck, formContext) {
+        console.log("api called");
+        if (DocTypeToCheck !== null && DocTypeToCheck !== "") {
+            var docTypes = DocTypeToCheck.split(",");
+            console.log("api called 1");
+            if (docTypes.length > 0) {
+                var fetchDocTypeCheck =
+                    "<fetch mapping='logical'>" +
+                    "  <entity name='wrmb_identificationdocument'>" +
+                    "	<attribute name='wrmb_identificationdocumentid' />" +
+                    "	<filter type='and'>" +
+                    "	  <condition attribute='statecode' operator='eq' value='0' />" +
+                    "	  <condition attribute='wrmb_companyid' operator='eq' value='" + formContext.data.entity.getId() + "' />" +
+                    "	</filter>" +
+                    "	<link-entity name='wrmb_identificationdocumenttype' from='wrmb_identificationdocumenttypeid' to='wrmb_typeid' alias='aq'>" +
+                    "	  <filter type='or'>";
+
+                for (var i = 0; i < docTypes.length; i++) {
+                    fetchDocTypeCheck += "<condition attribute='wrmb_name' operator='eq' value='" + docTypes[i].trim() + "' />";
+                }
+
+                fetchDocTypeCheck += "</filter>" +
+                    "	</link-entity>" +
+                    "  </entity>" +
+                    "</fetch>";
+
+                try {
+                    const retrievedDocs = await new Promise((resolve, reject) => {
+                        XrmServiceToolkit.Soap.Fetch(fetchDocTypeCheck, null, (result) => {
+                            resolve(result);
+                            console.log("api called 2");
+                        }, (error) => {
+                            reject(error);
+                            console.log("api called 3");
+                        });
+                    });
+
+                    if (retrievedDocs === null || retrievedDocs.length === 0) {
+                        formContext.ui.setFormNotification('No valid Identification Document assigned to this record!', 'WARNING', 'identitydocument_NOTIFY');
+                    }
+                } catch (error) {
+                    console.log(error)
+                }
+            }
+        }
+    }
 
     var onChangeCompanyType = function (executionContext) {
         "strict mode";
